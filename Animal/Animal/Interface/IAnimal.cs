12b4c0ad863e7a24eb78dfb886f101db5c01db51
Tatia Chichinadze <echichinadze@credo.ge>
2024-11-10 22:42:35 +0400

    new

---
+++ Animal/Animal/Interface/IAnimal.cs
@@ -0,0 +1,27 @@
+﻿using Animal.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal.Interface
+{
+    public interface IAnimal
+    {
+        string MakeSound(string text);
+    }
+    public class ConsoleAnimal : IAnimal
+    {
+
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+        public string MakeSound(string text)
+        {
+            return text;
+        }
+    }
+
+}
---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Animal
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public abstract string MakeSound();
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name, int age) : base(name, age) { }
+
+        public override string MakeSound()
+        {
+            return "Woof";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name, int age) : base(name, age) { }
+
+        public override string MakeSound()
+        {
+            return "Meow";
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public Cow(string name, int age) : base(name, age) { }
+
+        public override string MakeSound()
+        {
+            return "Moo";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Animal[] animals = new Animal[]
+            {
+                new Dog("Milo", 3),   
+                new Cat("Lusi", 2), 
+                new Cow("Chrela", 5)     
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.GetType().Name} says: {animal.MakeSound()}");
+            }
+        }
+    }
+}
