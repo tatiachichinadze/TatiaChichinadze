eaa5a17e3d25a8bb955515b16c982a55604fb82a
Tatia Chichinadze <echichinadze@credo.ge>
2024-11-14 23:08:11 +0400

    task10

---
+++ task 10/task 10/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+public class Fraction
+{
+    private int _numerator;   
+    private int _denominator; 
+
+  
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+            throw new DivideByZeroException();
+
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public override string ToString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+    {
+        var denominator = LCM(fraction1._denominator, fraction2._denominator);
+
+        var numerator = (denominator / fraction1._denominator * fraction1._numerator) +
+                        (denominator / fraction2._denominator * fraction2._numerator);
+
+        var gcd = GCD(denominator, numerator);
+
+        return new Fraction(numerator / gcd, denominator / gcd);
+    }
+
+    public static bool operator ==(Fraction fraction1, Fraction fraction2)
+    {
+        return (fraction1._numerator == fraction2._numerator) &&
+               (fraction1._denominator == fraction2._denominator);
+    }
+
+    public static bool operator !=(Fraction fraction1, Fraction fraction2)
+    {
+        return !(fraction1 == fraction2);
+    }
+
+    private static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private static int LCM(int a, int b)
+    {
+        return (a * b) / GCD(a, b);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Fraction fraction)
+        {
+            return this == fraction;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return _numerator.GetHashCode() ^ _denominator.GetHashCode();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var fraction1 = new Fraction(2, 8);
+        var fraction2 = new Fraction(3, 6);
+
+        var result = fraction1 + fraction2;
+
+        Console.WriteLine(result.ToString());
+    }
+}
+
